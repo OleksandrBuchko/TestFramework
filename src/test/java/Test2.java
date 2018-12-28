a8bae877309f2777467a8eb7ce0b70c4402f3e48
Oleksandr <oleksandr.buchko@lebara.com>
2018-12-28 22:10:16 +0200

    configure running tests

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,14 +1,12 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
 
 public class Test2 extends WebTest {
 
-    @Test
     public void testTest2(){
-        System.setProperty("webdriver.chrome.driver", "/src/main/java/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("www.chrome.com.ua");
+        driver.get("https://www.google.com.ua");
         driver.quit();
     }
 }
