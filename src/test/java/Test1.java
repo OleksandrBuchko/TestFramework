b881aa4b916acc17d9b6a6acafde304e3ddeb124
Oleksandr Buchko <oleksandr.buchko@gmail.com>
2019-01-02 21:54:12 +0200

    running tests in suite

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,8 +1,10 @@
+import io.qameta.allure.Step;
 import org.testng.annotations.Test;
 
 public class Test1 extends WebTest {
 
     @Test
+    @Step("Test1")
     public void testTest1() throws InterruptedException {
         //driver.get("https://www.google.com.ua");
         System.err.println("Test1");
